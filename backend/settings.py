b48c1aabf91a6580caada253ed8fa144e3f66cbd
Stephen Ekeh <stevenadibee@yahoo.com>
2025-07-09 11:39:38 +0000

    Fix database config for Render

--- backend/settings.py
+++ backend/settings.py
@@ -132,7 +132,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-   'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"), conn_max_age=600)
 }
 
 #DATABASES = {
